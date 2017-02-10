45507e3dfe1e6de04bd4bbe14750fb6cc7d246f4
sappy <sappy5678@gmail.com>
2017-02-10 15:03:09 +0800

    Add Test Framework mocha

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('Array',function(){
+    describe('#indexOf()',function(){
+        it('test test.js can work',function(){
+            assert.equal(-1,[1,2,3].indexOf(4));
+        });
+    });
+});
+
